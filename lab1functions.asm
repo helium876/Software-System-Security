0a4484d5ff2481c37bf009a4bcd772c487d32d70
helium96 <masters2020@gmail.com>
2017-01-23 10:41:34 -0500

    Lab 1 Functions

---
+++ lab1functions.asm
@@ -0,0 +1,28 @@
+;program constants
+SYS_WRITE equ 4
+SYS_EXIT equ 1
+STDOUT equ 1
+SAFE_EXIT equ 0
+
+section .data
+	hellomsg db "Hello World From Assembly", 0xa
+	msglength equ $ - hellomsg
+
+section .bss
+	varMsg resb 4
+	varLen resb 4 
+
+section .text
+	
+	global _start:
+
+	;print fucnction
+	printString:
+		;Write routine below
+		mov eax, SYS_WRITE
+		mov ebx, STDOUT
+		mov ecx, [varMsg]
+		mov edx, [varLen]
+		int 0x80
+
+	ret 
